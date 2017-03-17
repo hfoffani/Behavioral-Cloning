bb1471ee59ed4ca37d4cce718919693278ff0f66
Hernán M. Foffani <hfoffani@gmail.com>
2017-03-17 12:45:25 +0000

    LeNet

--- clone.py
+++ clone.py
@@ -30,18 +30,24 @@ y_train = np.array(measurements)
 print('...all pre processed')
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,5,5,activation="relu"))
+model.add(MaxPooling2D())
 model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train,
             validation_split=0.2,
             shuffle=True,
-            nb_epoch=2)
+            nb_epoch=5)
 
 model.save('model.h5')
 
