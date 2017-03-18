63af25e95edc3d838418bf9e605c1f484367bca0
Hernán M. Foffani <hfoffani@gmail.com>
2017-03-18 10:43:21 +0000

    Cropping2D is not compatible with Keras OSX

--- clone.py
+++ clone.py
@@ -30,10 +30,14 @@ y_train = np.array(measurements)
 print('...all pre processed')
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+# model.add(Cropping2D(cropping=((50,20), (1,1)), dim_ordering='tf'))
+
 model.add(Convolution2D(6,5,5,activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation="relu"))
