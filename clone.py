79cfc570f0c26c3cbe2530e611892c8f4ac04f58
Hernán M. Foffani <hfoffani@gmail.com>
2017-03-19 10:28:52 +0000

    NVIDIA model.

--- clone.py
+++ clone.py
@@ -33,23 +33,43 @@ print('...all pre processed')
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda
 from keras.layers import Convolution2D, MaxPooling2D
-from keras.layers import Cropping2D
+from keras.layers import Activation, Dropout
+from keras.optimizers import Adam
+
+def resize4nvidia(img):
+    import tensorflow as tf
+    return img
 
 model = Sequential()
-model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-# model.add(Cropping2D(cropping=((50,20), (1,1)), dim_ordering='tf'))
-model.add(Lambda(lambda x: x[:, :, 60:, :]))
+# model.add(Cropping2D . couldn't make it work
+model.add(Lambda(lambda x: x[:, :, 60:, :], input_shape=(160,320,3)))
+model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(resize4nvidia))
 
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation="relu"))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu")) 
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Convolution2D(64, 3, 3, activation="relu"))
+
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
+
+model.add(Dense(100))
+model.add(Activation('elu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(50))
+model.add(Activation('elu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(10))
+model.add(Activation('elu'))
+
 model.add(Dense(1))
 
-model.compile(loss='mse', optimizer='adam')
+
+model.compile(loss='mse',
+            optimizer=Adam(lr=0.0001))
 model.fit(X_train, y_train,
             validation_split=0.2,
             shuffle=True,
